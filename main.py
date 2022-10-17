9833f290898d65d6b2e6a0be9363068d24eed8a9
felixtsu <humaeks@gmail.com>
2022-10-17 12:58:34 +0800

    更新 main.ts, main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ def solveHanoi():
     Hanoi.pick_up(0) 
 
     # 这个函数可以将第2号当前拿在手里的盘子放到2好柱子上(最右边)
-    Hanoi.pick_up(2)
+    Hanoi.put_down_disk(2)
 
     # 这个函数可以拿到1号柱子(中间的)有几个盘
-    Hanoi.get_disk_number(1)
+    # Hanoi.get_disk_number(1)
 
     # 这个函数可以拿到1号柱子(中间的)最上面的盘的宽度，如果没有盘，就返回0
-    Hanoi.get_top_disk_length_of(1)
+    # Hanoi.get_top_disk_length_of(1)
 
     # 尝试写这个函数，自动的把碟片搬到最右边的柱子上
     pass
